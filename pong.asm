827e789886f53d66f66fadf877274da2d53e0296
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-11-01 08:50:21 +0100

    update comments

--- pong.asm
+++ pong.asm
@@ -15,9 +15,9 @@ clear_leds:
 ; BEGIN:set_pixel
 set_pixel:
 	andi $t0, $a0, 0x0003 ; save x modulo 4 in t0
-	sll $t0, $t0, 3 ; multiply t0 by 8
-	add $t0, $t0, $a1 ; add y to previous value. t0 should now be the pixel index within the word
+	sll $t0, $t0, 3 			; multiply t0 by 8
+	add $t0, $t0, $a1 		; add y to previous value. t0 should now be the pixel index within the word
 	andi $t1, $a0, 0x0012 ; mask the x coord to keep only values 0, 4 and 8
-	add $t1, LEDS, $t1 ; add the previous value to LEDS to find the correct LED word
+	add $t1, LEDS, $t1 		; add the previous value to LEDS to find the correct LED word
 	ret
 ; END:set_pixel
