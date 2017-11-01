636b3f0a1a32d9943b0e8a274bc9f618e9903e97
abaudat <baudatadrian@gmail.com>
2017-11-01 08:36:55 +0100

    Modified clear-leds and partially completed set-pixel

--- pong.asm
+++ pong.asm
@@ -6,8 +6,18 @@
 
 ; BEGIN:clear_leds
 clear_leds:
-	lui LEDS 0
-	lui LEDS+4 0
-	lui LEDS+8 0 
+	sw zero, LEDS
+	sw zero, LEDS+4
+	sw zero, LEDS+8
 	ret
 ; END:clear_leds
+
+; BEGIN:set_pixel
+set_pixel:
+	andi $t0, $a0, 0x0003 # save x modulo 4 in t0
+	sll $t0, $t0, 3 # multiply t0 by 8
+	add $t0, $t0, $a1 # add y to previous value. t0 should now be the pixel index within the word
+	andi $t1, $a0, 0x0012 # mask the x coord to keep only values 0, 4 and 8
+	add $t1, LEDS, $t1 # add the previous value to LEDS to find the correct LED word
+	ret
+; END:set_pixel
