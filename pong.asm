0637be747cd1cd3976264690ab6b6e1fe7d93202
abaudat <baudatadrian@gmail.com>
2017-11-01 08:00:09 +0100

    Completed cleat_leds procedure

--- pong.asm
+++ pong.asm
@@ -3,3 +3,11 @@
 .equ SCORES,  0x1018 ; game scores
 .equ LEDS,    0x2000 ; LED addresses
 .equ BUTTONS, 0x2030 ; Button addresses
+
+; BEGIN:clear_leds
+clear_leds:
+	lui LEDS 0
+	lui LEDS+4 0
+	lui LEDS+8 0 
+	ret
+; END:clear_leds
