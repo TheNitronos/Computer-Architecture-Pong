7ff3856d38f2ba408f610f10b046803c7b335a92
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-11-06 22:42:23 +0100

    space & co.

--- pong.asm
+++ pong.asm
@@ -64,25 +64,25 @@ clear_leds:
 
 ; BEGIN:set_pixel
 set_pixel:
-	andi t0, a0, 0x0003 	; save x modulo 4 in t0
-	slli t0, t0, 3 				; multiply t0 by 8
-	add t0, t0, a1 				; add y to previous value. t0 should now be the pixel index within the word
-	andi t1, a0, 0x000C 	; mask the x coord to keep only values 0, 4 and 8
-	addi t1, t1, LEDS 		; add the previous value to LEDS to find the correct LED word
-	ldw t3, 0(t1) 				; load the correct LED word in t3
+	andi t0, a0, 0x0003 ; save x modulo 4 in t0
+	slli t0, t0, 3 ; multiply t0 by 8
+	add t0, t0, a1 ; add y to previous value. t0 should now be the pixel index within the word
+	andi t1, a0, 0x000C ; mask the x coord to keep only values 0, 4 and 8
+	addi t1, t1, LEDS ; add the previous value to LEDS to find the correct LED word
+	ldw t3, 0(t1) ; load the correct LED word in t3
 	addi t2, zero, 0x0001
-	sll t2, t2, t0 				; prepare a mask for the correct pixel index in t2
-	or t3, t3, t2 				; set the correct pixel index to 1 via an or operation with t2
-	stw t3, 0(t1) 				; save the new word in the correct LED address
+	sll t2, t2, t0 ; prepare a mask for the correct pixel index in t2
+	or t3, t3, t2 ; set the correct pixel index to 1 via an or operation with t2
+	stw t3, 0(t1) ; save the new word in the correct LED address
 	ret
 
 ; END:set_pixel
 
 ; BEGIN:hit_test
 hit_test:
-	ldw t0, BALL(zero) 		; load the ball x position in t0
-	ldw t1, BALL+4(zero) 	; load the ball y position in t1
-	ldw t2, BALL+8(zero) 	; load the ball x velocity in t2
+	ldw t0, BALL(zero) ; load the ball x position in t0
+	ldw t1, BALL+4(zero) ; load the ball y position in t1
+	ldw t2, BALL+8(zero) ; load the ball x velocity in t2
 	ldw t3, BALL+12(zero) ; load the ball y velocity in t3
 walls:
 	addi t5, zero, 11 ; store the max x in t5
@@ -165,85 +165,81 @@ hit_end:
 
 ; BEGIN:move_ball
 move_ball:
-	ldw t0, BALL(zero) 		; load the ball x position in t0
-	ldw t1, BALL+4(zero) 	; load the ball y position in t1
-	ldw t2, BALL+8(zero) 	; load the ball x velocity in t2
+	ldw t0, BALL(zero) ; load the ball x position in t0
+	ldw t1, BALL+4(zero) ; load the ball y position in t1
+	ldw t2, BALL+8(zero) ; load the ball x velocity in t2
 	ldw t3, BALL+12(zero) ; load the ball y velocity in t3
-	add t0, t0, t2			 	; update x position
-	add t1, t1, t3 				; update y position
-	stw t0, BALL(zero) 		; store new ball x position
-	stw t1, BALL+4(zero) 	; store new ball y position
+	add t0, t0, t2			 ; update x position
+	add t1, t1, t3 ; update y position
+	stw t0, BALL(zero) ; store new ball x position
+	stw t1, BALL+4(zero) ; store new ball y position
 	ret
 
 ; END:move_ball
 
 ; BEGIN:move_paddles
 move_paddles:
-	ldw t0, PADDLES(zero) 	; load the left paddle y coord in t0
+	ldw t0, PADDLES(zero) ; load the left paddle y coord in t0
 	ldw t1, PADDLES+4(zero) ; load the right paddle y coord in t1
-	ldw t2, BUTTONS+4(zero)	; load the buttons edgecapture in t2
-	addi t4, zero, 1 				; minimum y coord
-	addi t5, zero, 6				; maximum y coord
-	andi t3, t2, 0x0001 		; mask to take only the last bit of t2 and store in t3 (this is left paddle up)
+	ldw t2, BUTTONS+4(zero) ; load the buttons edgecapture in t2
+	addi t4, zero, 1 ; minimum y coord
+	addi t5, zero, 6 ; maximum y coord
+	andi t3, t2, 0x0001 ; mask to take only the last bit of t2 and store in t3 (this is left paddle up)
 	beq t3, zero, no_left_paddle_up ; if no input, we don't want the paddle to move
-	beq t0, t4, no_left_paddle_up 	; if the paddle is already at the top it doesn't go up
-	addi t0, t0, -1 								; make the left paddle go up
-
+	beq t0, t4, no_left_paddle_up ; if the paddle is already at the top it doesn't go up
+	addi t0, t0, -1 ; make the left paddle go up
 no_left_paddle_up:
-	srli t2, t2, 1 										; shift buttons to the right to take the next button
-	andi t3, t2, 0x0001 							; mask to take only the last bit of t2 and store in t3 (this is left paddle down)
+	srli t2, t2, 1 ; shift buttons to the right to take the next button
+	andi t3, t2, 0x0001 ; mask to take only the last bit of t2 and store in t3 (this is left paddle down)
 	beq t3, zero, no_left_paddle_down ; if no input, we don't want the paddle to move
-	beq t0, t5, no_left_paddle_down 	; if the paddle is already at the bottom it doesn't go down
-	addi t0, t0, 1 										; make the left paddle go down
-
+	beq t0, t5, no_left_paddle_down ; if the paddle is already at the bottom it doesn't go down
+	addi t0, t0, 1 ; make the left paddle go down
 no_left_paddle_down:
-	srli t2, t2, 1 										; shift buttons to the right to take the next button
-	andi t3, t2, 0x0001 							; mask to take only the last bit of t2 and store in t3 (this is right paddle up)
-	beq t3, zero, no_right_paddle_up 	; if no input, we don't want the paddle to move
-	beq t1, t4, no_right_paddle_up 		; if the paddle is already at the top it doesn't go up
-	addi t1, t1, -1 									; make the right paddle go up
-
+	srli t2, t2, 1 ; shift buttons to the right to take the next button
+	andi t3, t2, 0x0001 ; mask to take only the last bit of t2 and store in t3 (this is right paddle up)
+	beq t3, zero, no_right_paddle_up ; if no input, we don't want the paddle to move
+	beq t1, t4, no_right_paddle_up ; if the paddle is already at the top it doesn't go up
+	addi t1, t1, -1 ; make the right paddle go up
 no_right_paddle_up:
-	srli t2, t2, 1 											; shift buttons to the right to take the next button
-	andi t3, t2, 0x0001 								; mask to take only the last bit of t2 and store in t3 (this is right paddle down)
-	beq t3, zero, no_right_paddle_down 	; if no input, we don't want the paddle to move
-	beq t1, t5, no_right_paddle_down 		; if the paddle is already at the top it doesn't go up
-	addi t1, t1, 1 											; make the left paddle go down
-
+	srli t2, t2, 1 ; shift buttons to the right to take the next button
+	andi t3, t2, 0x0001 ; mask to take only the last bit of t2 and store in t3 (this is right paddle down)
+	beq t3, zero, no_right_paddle_down ; if no input, we don't want the paddle to move
+	beq t1, t5, no_right_paddle_down ; if the paddle is already at the top it doesn't go up
+	addi t1, t1, 1 ; make the left paddle go down
 no_right_paddle_down:
-	stw t0, PADDLES(zero) 							; store the new right paddle y coord
-	stw t1, PADDLES+4(zero) 						; store the new left paddle y coord
+	stw t0, PADDLES(zero) ; store the new right paddle y coord
+	stw t1, PADDLES+4(zero) ; store the new left paddle y coord
 	srli t2, t2, 1
-	slli t2, t2, 4 											; replace the last 4 bits of buttons edgecapture with 0
-	stw t2, BUTTONS+4(zero) 						; reset buttons edgecapture last 4 bits
+	slli t2, t2, 4 ; replace the last 4 bits of buttons edgecapture with 0
+	stw t2, BUTTONS+4(zero) ; reset buttons edgecapture last 4 bits
 	ret
 
 ; END:move_paddles
 
 ; BEGIN:draw_paddles
 draw_paddles:
-	addi sp, sp, -12 				; make room for 3 items on stack
+	addi sp, sp, -12 ; make room for 3 items on stack
 	stw ra, 8(sp) ; push ra on stack
-	stw a0, 4(sp) 					; push a0 on stack
-	stw a1, 0(sp) 					; push a1 on stack
-	ldw a1, PADDLES(zero) 	; load the left paddle y coord in a1
-	addi a0, zero, 0 				; store the left paddle x coord in a0
-	call set_pixel 					; draw the middle pixel of the left paddle
+	stw a0, 4(sp) ; push a0 on stack
+	stw a1, 0(sp) ; push a1 on stack
+	ldw a1, PADDLES(zero) ; load the left paddle y coord in a1
+	addi a0, zero, 0 ; store the left paddle x coord in a0
+	call set_pixel ; draw the middle pixel of the left paddle
 	addi a1, a1, 1
-	call set_pixel 					; draw the bottom pixel of the left paddle
+	call set_pixel ; draw the bottom pixel of the left paddle
 	addi a1, a1, -2
-	call set_pixel 					; draw the top pixel of the left paddle
+	call set_pixel ; draw the top pixel of the left paddle
 	ldw a1, PADDLES+4(zero) ; load the right paddle y coord in a1
-	addi a0, zero, 11 			; store the right paddle x coord in a0
-	call set_pixel 					; draw the middle pixel of the right paddle
+	addi a0, zero, 11 ; store the right paddle x coord in a0
+	call set_pixel ; draw the middle pixel of the right paddle
 	addi a1, a1, 1
-	call set_pixel 					; draw the bottom pixel of the right paddle
+	call set_pixel ; draw the bottom pixel of the right paddle
 	addi a1, a1, -2
-	call set_pixel 					; draw the top pixel of the right paddle
+	call set_pixel ; draw the top pixel of the right paddle
 	ldw ra, 8(sp) ; preserve ra on call
-	ldw a0, 4(sp) 					; preserve a0 on call
-	ldw a1, 0(sp) 					; preserve a1 on call
-	addi sp, sp, 12				; hand back stack space
+	ldw a0, 4(sp) ; preserve a0 on call
+	ldw a1, 0(sp) ; preserve a1 on call
+	addi sp, sp, 12 ; hand back stack space
 	ret
 
 ; END:draw_paddles
@@ -316,6 +312,7 @@ get_font:
 	ldw v0, font_data+60(zero) ; F
 font_end:
 	ret
+
 ; END:display_score
 
 wait:
@@ -326,7 +323,6 @@ loop:
 	bne t0, zero, loop
 end_loop:
 	ret
-
 
 font_data:
 .word 0x7E427E00 ; 0
